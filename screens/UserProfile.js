132071320a9f05f6091325cef80eab21cf5d7e25
Joe <jppgibbs@gmail.com>
2020-03-19 21:56:12 +0000

    Login Page Initial CSS

--- screens/UserProfile.js
+++ screens/UserProfile.js
@@ -1,5 +1,13 @@
 import React, {Component} from 'react';
-import {Text, TextInput, View, Alert, Button, StyleSheet} from 'react-native';
+import {
+  Text,
+  TextInput,
+  View,
+  Alert,
+  Button,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 
 class Login extends Component {
@@ -21,54 +29,68 @@ class Login extends Component {
     try {
       await AsyncStorage.setItem('user_id', JSON.stringify(this.state.user_id));
       let user_id = await AsyncStorage.getItem('user_id');
+
+      console.log('Stored in async. UID:' + user_id);
+    } catch (error) {
+      console.log('Failed to store user. ' + error);
+    }
+  }
+
+  async storeToken() {
+    try {
       await AsyncStorage.setItem('x_auth', JSON.stringify(this.state.x_auth));
       let x_auth = await AsyncStorage.getItem('x_auth');
 
-      console.log('Stored in async. UID:' + user_id + ' xauth:' + x_auth);
+      console.log('xauth: ' + x_auth);
     } catch (error) {
-      console.log('Failed to store user. ' + error);
+      console.log('Failed to store token. ' + error);
     }
   }
 
   // Login
   login = () => {
     console.log('Debug: Attempting login...');
-    return fetch('http://10.0.2.2:3333/api/v0.0.5/login', {
-      method: 'POST',
-      body: JSON.stringify({
-        email: this.state.email,
-        password: this.state.password,
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then(response => {
-        // !!: Possibly redundant
-        // Handle 2000 reply, if login was invalid
-        if (response.status !== 200) {
-          console.log('Debug: invalid login');
-        }
-        return response.json();
+    return (
+      fetch('http://10.0.2.2:3333/api/v0.0.5/login', {
+        method: 'POST',
+        body: JSON.stringify({
+          email: this.state.email,
+          password: this.state.password,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
       })
-      .then(responseJson => {
-        // Login Success
-        this.props.navigation.navigate('PostChits');
-        this.setState({
-          user_id: JSON.stringify(responseJson.id),
-          x_auth: JSON.stringify(responseJson.token),
-        });
-        console.log('Login successful');
-        this.storeUser();
-        Alert.alert('Login Successful');
-      })
-      .catch(error => {
-        console.log('Login error: \n' + error);
-        this.setState({
-          loggedIn: true,
-          validation: 'Please enter a valid login',
-        });
-      });
+        .then(response => {
+          // !!: Possibly redundant
+          // Handle 2000 reply, if login was invalid
+          // if (response.status !== 200) {
+          //   console.log('Debug: invalid login');
+          // }
+          return response.json();
+        })
+        .then(responseJson => {
+          // Login Success
+          this.props.navigation.navigate('PostChits');
+          this.setState({
+            user_id: JSON.stringify(responseJson.id),
+            x_auth: JSON.stringify(responseJson.token),
+          });
+          console.log('Login successful');
+          this.storeUser();
+          this.storeToken();
+          Alert.alert('Login Successful');
+        })
+        // If the login fails
+        .catch(error => {
+          console.log('Login error: \n' + error);
+          Alert.alert('Login Failed');
+          this.setState({
+            loggedIn: true,
+            validation: 'Please enter a valid login',
+          });
+        })
+    );
   };
 
   render() {
@@ -78,7 +100,7 @@ class Login extends Component {
 
         <Text>Email Address</Text>
         <TextInput
-          style={styles.textInput}
+          style={styles.textEntry}
           onChangeText={text => this.setState({email: text})}
           value={this.state.email}
           defaultValue="test@test.com"
@@ -91,6 +113,7 @@ class Login extends Component {
         />
         <Text>Password</Text>
         <TextInput
+          style={styles.textEntry}
           onChangeText={text => this.setState({password: text})}
           value={this.state.password}
           placeholder="Password"
@@ -101,13 +124,19 @@ class Login extends Component {
           accessibilityLabel="Enter password"
           accessibilityHint="Enter the password for the account you wish to login to"
         />
-        <Button title="Log in" onPress={() => this.login()} />
-        <Button
-          title="Create Account"
-          onPress={() => {
-            this.createAccount();
-          }}
-        />
+        <TouchableOpacity onPress={() => this.login()} style={styles.button}>
+          <Text>Log In</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => this.createAccount()}
+          style={styles.button}>
+          <Text>Create Account</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => this.createAccount()}
+          style={styles.button}>
+          <Text>Continue Without Logging In</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -118,11 +147,30 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     flex: 1,
     backgroundColor: 'aliceblue',
+    margin: 15,
   },
   button: {
     alignItems: 'center',
+    elevation: 2,
     padding: 10,
-    paddingTop: 10,
+    marginTop: 5,
+    marginBottom: 0,
+    borderColor: '#101010',
+    borderWidth: 1,
+    borderRadius: 2,
+    backgroundColor: '#2296f3',
+  },
+  textEntry: {
+    alignItems: 'center',
+    padding: 5,
+
+    marginTop: 5,
+    marginBottom: 0,
+    borderColor: '#2296f3',
+    borderRadius: 2,
+    borderWidth: 1,
+    backgroundColor: '#ffffff',
+    elevation: 3,
   },
   title: {
     fontSize: 18,
