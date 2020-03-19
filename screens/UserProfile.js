1307c258542071f482f68ff484fc04d55991a88e
Joe <jppgibbs@gmail.com>
2020-03-19 19:55:55 +0000

    Login Async Token Implementation
    - Async xauth token
    - UI improvements

--- screens/UserProfile.js
+++ screens/UserProfile.js
@@ -1,208 +1,115 @@
 import React, {Component} from 'react';
-import {
-  Text,
-  TextInput,
-  View,
-  Alert,
-  Button,
-  FlatList,
-  StyleSheet,
-} from 'react-native';
+import {Text, TextInput, View, Alert, Button, StyleSheet} from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 
-class UserProfile extends Component {
-
+class Login extends Component {
   // Construct variables with default empty values
   constructor(props) {
     super(props);
     this.state = {
-      given_name: '',
-      family_name: '',
       email: '',
       password: '',
       loggedIn: false,
-      userID: '',
-      profileData: [],
+      user_id: '',
+      x_auth: '',
+      validation: '',
     };
   }
 
-  // Store UserID
-  storeUserId = async id => { // !!: storeID -> storeUserId
+  // Store user in async
+  async storeUser() {
     try {
-      // Get ID then Convert to JSON
-      console.log('id = ', id);
-      await AsyncStorage.setItem('@id', JSON.stringify(id)); 
+      await AsyncStorage.setItem('user_id', JSON.stringify(this.state.user_id));
+      let user_id = await AsyncStorage.getItem('user_id');
+      await AsyncStorage.setItem('x_auth', JSON.stringify(this.state.x_auth));
+      let x_auth = await AsyncStorage.getItem('x_auth');
+
+      console.log('Stored in async. UID:' + user_id + ' xauth:' + x_auth);
     } catch (error) {
-      console.log(error);
+      console.log('Failed to store user. ' + error);
     }
-  };
+  }
 
-  storeLogInToken = async token => {
-    try {
-      console.log('Token:', token);
-      await AsyncStorage.setItem('@logintoken', token);
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
-  retrieveID = async () => {
-    try {
-      const value = await AsyncStorage.getItem('@id');
-      if (value !== null) {
-        this.setState({userID: value});
-        console.log(this.state.userID);
-      }
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
-  logIn() {
-    let res = JSON.stringify({
-      email: this.state.email,
-      password: this.state.password,
-    });
-
-    console.log(res);
-
+  // Login
+  login = () => {
+    console.log('Debug: Attempting login...');
     return fetch('http://10.0.2.2:3333/api/v0.0.5/login', {
       method: 'POST',
-      body: res,
+      body: JSON.stringify({
+        email: this.state.email,
+        password: this.state.password,
+      }),
       headers: {
         'Content-Type': 'application/json',
       },
     })
       .then(response => {
-        let res = response.json();
-        return res;
+        // !!: Possibly redundant
+        // Handle 2000 reply, if login was invalid
+        if (response.status !== 200) {
+          console.log('Debug: invalid login');
+        }
+        return response.json();
       })
       .then(responseJson => {
-        console.log(responseJson.token);
-        Alert.alert('Logged in');
-        this.getProfileData(() => {
-          this.storeUserId(responseJson.id);
-          this.storeLogInToken(responseJson.token);
-          this.setState({
-            loggedIn: true,
-          });
-          console.log(this.state.loggedIn);
+        // Login Success
+        this.props.navigation.navigate('PostChits');
+        this.setState({
+          user_id: JSON.stringify(responseJson.id),
+          x_auth: JSON.stringify(responseJson.token),
         });
+        console.log('Login successful');
+        this.storeUser();
+        Alert.alert('Login Successful');
       })
       .catch(error => {
-        console.error(error);
+        console.log('Login error: \n' + error);
+        this.setState({
+          loggedIn: true,
+          validation: 'Please enter a valid login',
+        });
       });
-  }
-
-  createAccount() {
-    let res1 = JSON.stringify({
-      given_name: this.state.given_name,
-      family_name: this.state.family_name,
-      email: this.state.email,
-      password: this.state.password,
-    });
-
-    console.log(res1);
-
-    return fetch('http://10.0.2.2:3333/api/v0.0.5/user', {
-      method: 'POST',
-      body: res1,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then(response => {
-        Alert.alert('Account Created');
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }
-
-  getProfileData(done) {
-    return fetch('http://10.0.2.2:3333/api/v0.0.5/user/' + this.state.userID, {
-      method: 'GET',
-    })
-      .then(response => response.json())
-      .then(responseJson => {
-        this.setState(
-          {
-            profileData: responseJson,
-          },
-          () => {
-            done();
-          },
-        );
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
-
-  componentDidMount() {
-    this.retrieveID();
-  }
+  };
 
   render() {
-    if (this.state.loggedIn) {
-      return (
-        <View style={styles.viewStyle}>
-          <Text>
-            {this.state.profileData.given_name}{' '}
-            {this.state.profileData.family_name}
-          </Text>
-          <Text>Chits</Text>
-          <FlatList
-            data={this.state.profileData.recent_chits}
-            renderItem={({item}) => <Text>{item.chit_content}</Text>}
-            keyExtractor={({id}, index) => id}
-          />
-        </View>
-      );
-    } else {
-      return (
-        <View style={styles.viewStyle}>
-          <Text>First Name</Text>
-          <TextInput
-            onChangeText={text => this.setState({given_name: text})}
-            value={this.state.given_name}
-          />
+    return (
+      <View style={styles.viewStyle}>
+        <Text style={styles.title}>Login</Text>
 
-          <Text>Second Name</Text>
-          <TextInput
-            onChangeText={text => this.setState({family_name: text})}
-            value={this.state.family_name}
-          />
-
-          <Text>Email</Text>
-          <TextInput
-            style={styles.textInput}
-            onChangeText={text => this.setState({email: text})}
-            value={this.state.email}
-            textContentType="emailAddress"
-          />
-          <Text>Password</Text>
-          <TextInput
-            onChangeText={text => this.setState({password: text})}
-            value={this.state.password}
-            secureTextEntry
-          />
-
-          <Button
-            title="Log In"
-            onPress={() => {
-              this.logIn();
-            }}
-          />
-          <Button
-            title="Create Account"
-            onPress={() => {
-              this.createAccount();
-            }}
-          />
-        </View>
-      );
-    }
+        <Text>Email Address</Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={text => this.setState({email: text})}
+          value={this.state.email}
+          defaultValue="test@test.com"
+          placeholder="example@example.com"
+          textContentType="emailAddress"
+          accessibilityComponentType="none"
+          accessibilityRole="none"
+          accessibilityLabel="Enter email"
+          accessibilityHint="Enter the email for the account you wish to login to"
+        />
+        <Text>Password</Text>
+        <TextInput
+          onChangeText={text => this.setState({password: text})}
+          value={this.state.password}
+          placeholder="Password"
+          defaultValue="test"
+          secureTextEntry
+          accessibilityComponentType="none"
+          accessibilityRole="none"
+          accessibilityLabel="Enter password"
+          accessibilityHint="Enter the password for the account you wish to login to"
+        />
+        <Button title="Log in" onPress={() => this.login()} />
+        <Button
+          title="Create Account"
+          onPress={() => {
+            this.createAccount();
+          }}
+        />
+      </View>
+    );
   }
 }
 
@@ -212,6 +119,14 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'aliceblue',
   },
+  button: {
+    alignItems: 'center',
+    padding: 10,
+    paddingTop: 10,
+  },
+  title: {
+    fontSize: 18,
+  },
 });
 
-export default UserProfile;
+export default Login;
